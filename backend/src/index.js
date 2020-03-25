8dd7e4caa200cb0bf65a77a86598cd45d2360e0e
x <x@x.x>
2020-03-25 16:16:37 +0200

    initial commit: backend: setup and register
     Committer: wschwab <wschwab@pm.me>

---
+++ backend/src/index.js
@@ -0,0 +1,44 @@
+require('dotenv/config')
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const { verify } = require('jsonwebtoken')
+const { hash, compare } = require('bcryptjs')
+
+const app = express()
+
+app.use(cookieParser())
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  })
+)
+
+// Read body data in JSON or URL
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Register
+app.post('/register', async (req, res) => {
+  const { email, password } = req.body
+
+  try {
+    const user = fakeDB.find(user => user.email === email)
+    if (user) return res.status(400).json({ body: 'User already exists'})
+    const hashedPassword = await hash(password, 10)
+    fakeDB.push({
+      id: fakeDB.length,
+      email,
+      password: hashedPassword
+    })
+    res.send({ message: 'User Created' })
+    console.log(fakeDB)
+  } catch (err => {
+      console.error(err);
+      res.status(500).json({ error: err.code });
+  })
+})
+
+// Login
