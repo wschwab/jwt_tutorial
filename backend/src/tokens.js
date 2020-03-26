13002e25ab8db9548f282ed128d29a2a09f9d6b1
wschwab <wschwab@pm.me>
2020-03-26 17:09:41 +0200

    backend: fixed problems, login works

--- backend/src/tokens.js
+++ backend/src/tokens.js
@@ -1,15 +1,15 @@
 const { sign } = require('jsonwebtoken')
 
 const createAccessToken = userId => {
-  return sign({ userId }), process.env.ACCESS_TOKEN_SECRET, {
+  return sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: '15m'
-  }
+  })
 }
 
 const createRefreshToken = userId => {
-  return sign({ userId }), process.env.REFRESH_TOKEN_SECRET, {
+  return sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
     expiresIn: '7d'
-  }
+  })
 }
 
 const sendAccessToken = (req, res, accesstoken) => {
@@ -20,7 +20,7 @@ const sendAccessToken = (req, res, accesstoken) => {
 }
 
 // would want the token to have a more obfuscated name in production
-const sendRefreshToken = (res, refreshtoken) => {
+const sendRefreshToken = (res, token) => {
   res.cookie('refreshtoken', token, {
     httpOnly: true,
     path: '/refresh_token'
